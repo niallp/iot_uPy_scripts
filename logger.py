3dffe7315059af8d3f54469c2a1f5a5acc25cf60
Niall Parker <stuff@npengineering.ca>
2017-03-06 21:10:20 -0800

    add board specific scale factor

--- logger.py
+++ logger.py
@@ -13,18 +13,20 @@ uniq = machine.unique_id()
 if 'ESP8266' in mch:
     if uniq[0] == 0x5d:
         brdName = 'esp12a'
+        adcScl = 548
     else:
         brdName = 'esp12b'
+        adcScl = 531
 else:
     print("Board not recognized, for ESP2866 only now")
     raise SystemExit
 
 # account for voltage divider of 100k over 22k on Vinput: tweaked to calibrate
-def vin_str(adc):
+def vin_str(adc,scl):
     acc = 0
     for r in range(10):     # average over 10 in case of noise
         acc += adc()
-    milliVolts = ((acc-25)*548) // 1024
+    milliVolts = ((acc-25)*scl) // 1024
     return str(milliVolts // 1000) + "." + "{0:0>3}".format(milliVolts % 1000)
 
 #setup 
@@ -42,7 +44,7 @@ s.connect(addr)
 s.send(mqtt.connect(brdName))
 print("connecting to broker")
 time.sleep(1)           # allow connection setup
-vStr = vin_str(vin)
+vStr = vin_str(vin,adcScl)
 s.send(mqtt.pub(brdName+"/vin",vStr))
 print('send:'+brdName+"/vin/"+vStr)
 time.sleep(1)
