f8e796cf331548c3ff89cd04a922f537ff2ed35d
Niall Parker <stuff@npengineering.ca>
2017-03-06 19:59:07 -0800

    working version of deepsleep

--- logger.py
+++ logger.py
@@ -19,45 +19,35 @@ else:
     print("Board not recognized, for ESP2866 only now")
     raise SystemExit
 
-# for periodic publishing
-def publish_handler(rtc_o):
-    global pubCount
-    p_usr.value(0)
-    pubCount += 1
-    p_usr.value(1)
-
-# account for voltage divider of 115k over 56k on Vinput
+# account for voltage divider of 100k over 22k on Vinput: tweaked to calibrate
 def vin_str(adc):
-    milliVolts = (adc()*171*1100*4) // (3*56*4096)
-    return str(milliVolts // 1000) + "." + str(milliVolts % 1000)
+    acc = 0
+    for r in range(10):     # average over 10 in case of noise
+        acc += adc()
+    milliVolts = ((acc-25)*548) // 1024
+    return str(milliVolts // 1000) + "." + "{0:0>3}".format(milliVolts % 1000)
 
 #setup 
-p_usr = machine.Pin(13, mode=machine.Pin.OUT)
-p_usr.value(0)
 adc = machine.ADC(0)
 vin = adc.read
-
-pubCount = 0
-nextCount = pubCount
-
-# setup interrupt handler to publish once every period
-tmr = machine.Timer(-1)
-tmr.init(period=10000, mode=machine.Timer.PERIODIC, callback=publish_handler)
+# configure wakeup
+rtc = machine.RTC()
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+rtc.alarm(rtc.ALARM0,60000)
 
 #open socket and send first message
 addr = socket.getaddrinfo("pogoplug",1883)[0][4]
 s = socket.socket()
 s.connect(addr)
 s.send(mqtt.connect(brdName))
-
+print("connecting to broker")
+time.sleep(1)           # allow connection setup
+vStr = vin_str(vin)
+s.send(mqtt.pub(brdName+"/vin",vStr))
+print('send:'+brdName+"/vin/"+vStr)
 time.sleep(1)
+s.send(mqtt.disconnect())
+s.close()
 
-s.send(mqtt.pub(brdName,b"starting:"+str(pubCount)))
-
-while True:
-    s.send(mqtt.pub(brdName+"/iter",str(pubCount)))
-    vStr = vin_str(vin)
-    s.send(mqtt.pub(brdName+"/vin",vStr))
-    nextCount += 1
-    while nextCount > pubCount:
-        machine.idle()
+print('back to sleep')
+machine.deepsleep()     # back to sleep
