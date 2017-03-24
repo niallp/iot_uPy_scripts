90fb7ee8aabc6c164db51dcdb45cc53bb386d068
Niall Parker <stuff@npengineering.ca>
2017-03-24 00:09:03 -0700

    modularize wipy and shift to 1 min reports

--- logger.py
+++ logger.py
@@ -1,22 +1,21 @@
 # basic data logger using WiPy
-# fork for esp2866 modules
+# fork back to wipy
 # Niall Parker, 7apr2016
+from machine import Pin
 import socket
 import time
 import machine
 import os
+import wipy
 
 import mqtt
 
 mch = os.uname().machine
 uniq = machine.unique_id()
-if 'ESP8266' in mch:
-    if uniq[0] == 0x5d:
-        brdName = 'esp12a'
-    else:
-        brdName = 'esp12b'
+if 'WiPy' in mch:
+    brdName = 'wipy'
 else:
-    print("Board not recognized, for ESP2866 only now")
+    print("wipy only now")
     raise SystemExit
 
 # for periodic publishing
@@ -26,23 +25,32 @@ def publish_handler(rtc_o):
     pubCount += 1
     p_usr.value(1)
 
+# returns temperature in degree C from LM35 as string
+def lm35C_str(adc):
+    milliVolts = (adc()*1100*4) // (3 * 4096)
+    return str(milliVolts // 10) + "." + str(milliVolts % 10)
+
 # account for voltage divider of 115k over 56k on Vinput
 def vin_str(adc):
     milliVolts = (adc()*171*1100*4) // (3*56*4096)
     return str(milliVolts // 1000) + "." + str(milliVolts % 1000)
 
 #setup 
-p_usr = machine.Pin(13, mode=machine.Pin.OUT)
-p_usr.value(0)
-adc = machine.ADC(0)
-vin = adc.read
+wipy.heartbeat(False)
+p_usr = Pin('GP16', mode=Pin.OUT)
+p_usr.value(1)		# turn off user LED
+
+adc = machine.ADC()
+vin = adc.channel(pin='GP3')
+lm35 = adc.channel(pin='GP5')
 
 pubCount = 0
 nextCount = pubCount
 
-# setup interrupt handler to publish once every period
-tmr = machine.Timer(-1)
-tmr.init(period=10000, mode=machine.Timer.PERIODIC, callback=publish_handler)
+# setup RTC interrupt handler to publish once every period
+rtc = machine.RTC()
+rtc_i = rtc.irq(trigger=machine.RTC.ALARM0, handler=publish_handler, wake=machine.IDLE)
+rtc.alarm(time=60000, repeat=True)
 
 #open socket and send first message
 addr = socket.getaddrinfo("pogoplug",1883)[0][4]
@@ -56,8 +64,8 @@ s.send(mqtt.pub(brdName,b"starting:"+str(pubCount)))
 
 while True:
     s.send(mqtt.pub(brdName+"/iter",str(pubCount)))
-    vStr = vin_str(vin)
-    s.send(mqtt.pub(brdName+"/vin",vStr))
+    s.send(mqtt.pub(brdName+"/temp",lm35C_str(lm35)))
+    s.send(mqtt.pub(brdName+"/vin",vin_str(vin)))
     nextCount += 1
     while nextCount > pubCount:
         machine.idle()
