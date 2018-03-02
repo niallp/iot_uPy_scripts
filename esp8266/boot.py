af2a8ae9298e9b185b1a117531008e29af6ab507
Niall Parker <stuff@npengineering.ca>
2018-03-01 20:52:35 -0800

    add forced connection to balskG on boot

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -1,9 +1,22 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('balskG', '')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
 import esp
 esp.osdebug(None)
 import machine
 cause = machine.reset_cause()
 print("Reset cause: "+str(cause))
+do_connect()
 if (cause == machine.PWRON_RESET) or (cause == machine.DEEPSLEEP_RESET):
     exec(open('logger.py').read())
 else:
