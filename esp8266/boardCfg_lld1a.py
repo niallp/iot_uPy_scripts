0d923d02324479b9a4f6fce528204cebcc217163
Niall Parker <stuff@npengineering.ca>
2018-08-20 15:20:16 -0700

    make IP for station variable also new cfgs for Lolin boards

--- esp8266/boardCfg_lld1a.py
+++ esp8266/boardCfg_lld1a.py
@@ -1,11 +1,11 @@
-brdName = 'esp12d'
-userToken = 'eubEzHnrUFsFOaT9uNjS'
-adcScl = 529
+brdName = 'lld1a'
+userToken = 'J2qBiBWLm6rw62sNtd6W'
+adcScl = 438
 mqttHost = 'thingsboard'
 known_nets = {
     'balskG': {'pwd': None},
     'exceptional': {'pwd': None},
     'Community Hall': {'pwd': None},
-    'rcmsar20': {'pwd': None, 'wlan_config': ('192.168.20.54', '255.255.255.0', '192.168.20.1', '192.168.20.1')},
+    'rcmsar20': {'pwd': None, 'wlan_config': ('192.168.20.56', '255.255.255.0', '192.168.20.1', '192.168.20.1')},
     'Port Browning Marina': {'pwd': 'cra175tb34'}
 }
--- esp8266/boardCfg_lld1b.py
+++ esp8266/boardCfg_lld1b.py
@@ -1,11 +1,11 @@
-brdName = 'esp12d'
-userToken = 'eubEzHnrUFsFOaT9uNjS'
-adcScl = 529
+brdName = 'lld1b'
+userToken = 'syHqUCjmlkVuFL74CKsC'
+adcScl = 438
 mqttHost = 'thingsboard'
 known_nets = {
     'balskG': {'pwd': None},
     'exceptional': {'pwd': None},
     'Community Hall': {'pwd': None},
-    'rcmsar20': {'pwd': None, 'wlan_config': ('192.168.20.54', '255.255.255.0', '192.168.20.1', '192.168.20.1')},
+    'rcmsar20': {'pwd': None, 'wlan_config': ('192.168.20.55', '255.255.255.0', '192.168.20.1', '192.168.20.1')},
     'Port Browning Marina': {'pwd': 'cra175tb34'}
 }
