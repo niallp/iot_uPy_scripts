de362a571702adeb0ec641cb1c10916255f309aa
Niall Parker <stuff@npengineering.ca>
2019-05-19 21:06:02 -0700

    use strongest ap in known_nets, also debug LED on p13

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -1,17 +1,39 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 
+# should now try strongest ap it knows about, default to balskG
 def do_connect():
+    from machine import Pin
+    con = Pin(13,Pin.OUT)
     import network
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False)
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        con.value(1)
+        bssid = None
+        pwd = None
+        nets = sta_if.scan()
+        nets.sort(key=lambda net: net[3], reverse=True)     # strongest first
+        from boardCfg import known_nets
+        for net in nets:
+            ssid = net[0].decode('utf-8')
+            if ssid in known_nets:
+                bssid = net[1]
+                pwd = known_nets[ssid]['pwd']
+                break
+            else:
+                print(ssid+" is unknown")
+        print('connecting to network '+ssid)
         sta_if.active(True)
-        sta_if.connect('balskG', '')
+        if bssid == None:
+            sta_if.connect(ssid,pwd)
+        else:
+            sta_if.connect(ssid,pwd,bssid=bssid)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
+    if sta_if.isconnected():
+        con.value(0)
 
 import esp
 esp.osdebug(None)
@@ -26,6 +48,7 @@ if (cause != machine.DEEPSLEEP_RESET):
     webrepl.start()
     gc.collect()
     from utime import sleep
+    print('Ctrl-C to intervene in autostart of logger')
     sleep(120)
 
 exec(open('logger.py').read())
