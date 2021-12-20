97fcfdf92572ff900ef3859de197e873a367fb9d
Niall Parker <stuff@npengineering.ca>
2021-12-19 21:53:55 -0800

    a netConfig template

---
+++ netConfig-template.py
@@ -0,0 +1,7 @@
+mqttHost = 'thingsboard.balsk.ca'
+known_nets = {
+    'balskG': {'pwd': None},
+    'Community Hall': {'pwd': None},
+    'SHAW-EFD0E0': {'pwd': 'somePwd'},
+    'Port Browning Marina Guest': {'pwd': 'someOtherPwd'},
+}
