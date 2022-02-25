3de9e7653d91933ba2fab0f73ccdad27a4b48e28
Niall Parker <stuff@npengineering.ca>
2022-02-25 10:23:39 -0800

    new sensor node

---
+++ esp8266/boardCfg_proD1h.py
@@ -0,0 +1,10 @@
+brdName = 'proD1h'
+userToken = 'cKuRnR8r9zQ8QtB4pnGm'
+adcScl = 442
+mqttHost2 = None
+mqttTopic2 = None
+highPin = None
+lowPin = None
+oneWirePin = 12
+dhtPin = None
+sht30Pins = (4, 5)
