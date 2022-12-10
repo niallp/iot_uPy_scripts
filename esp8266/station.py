1b2f997080d3c5a99931b5cd845e712da1173d6a
Niall Parker <stuff@npengineering.ca>
2022-12-09 22:43:53 -0800

    more temperature sensors, rework detection and sleep, include relay control

--- esp8266/station.py
+++ esp8266/station.py
@@ -12,8 +12,14 @@ from boardCfg import adcScl
 from boardCfg import userToken
 from netConfig import mqttHost
 from boardCfg import mqttHost2
-
-import sht30
+from boardCfg import mqttTopic2
+from boardCfg import highPin
+from boardCfg import lowPin
+from boardCfg import oneWirePin
+from boardCfg import dhtPin
+from boardCfg import sht30Pins
+from boardCfg import minTime
+from boardCfg import nomVolts
 
 # account for voltage divider of 100k over 22k on Vinput: tweaked to calibrate
 def vin_mV(adc,scl):
@@ -23,17 +29,71 @@ def vin_mV(adc,scl):
     milliVolts = ((acc-25)*scl) // 1024
     return milliVolts
 
-def milli_str(milli):
-    return str(milli // 1000) + "." + "{0:0>3}".format(milli % 1000) 
+def milli_str(millis):
+    return str(millis // 1000) + "." + "{0:0>3}".format(millis % 1000) 
+
+def pin_str(gpio):
+    # default switches pulled high so active low
+    pin = machine.Pin(gpio,machine.Pin.IN,machine.Pin.PULL_UP)
+    return '1' if pin.value() == 0 else '0'
+
+def relayCtl(topic, msg):
+    global relay
+    print("Topic: "+str(topic.decode())+" Msg: "+str(msg.decode()))
+    relay.value(int(msg))
 
 #setup 
 adc = machine.ADC(0)
-milliVolts = vin_mV(adc.read,adcScl)
-sleepTime = 20000
+milliVolts = vin_mV(adc.read,adcScl)    # want to sleep longer if voltage is low
+sleepTime = minTime*1000
 ctlFlg = True
+if milliVolts < nomVolts*9/10:
+    sleepTime = sleepTime*3
+if milliVolts < nomVolts*8/10:
+    sleepTime = sleepTime*3
+if milliVolts < nomVolts*7/10:
+    sleepTime = sleepTime*10 
 
-sense1 = sht30.SHT30()
-sense2 = sht30.SHT30(i2c_address=sht30.ALTERNATE_I2C_ADDRESS)
+if sleepTime > 4200000:
+    sleepTime = 4200000    # around 70 minutes (max RTC timeout) if very low battery
+
+# configure sensors
+if oneWirePin is not None:
+    import onewire
+    import ds18x20
+    ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(oneWirePin)))
+    roms = ds.scan()
+    if len(roms) == 0:
+        tempFlg = False
+        print('DS18x20 not found')
+    else:
+        ds.convert_temp()
+        tempFlg = True
+else:
+    tempFlg = False
+
+if dhtPin is not None:
+    import dht
+    ht = dht.DHT22(machine.Pin(dhtPin))
+    try:
+        ht.measure()
+        dhFlag = True
+    except:
+        dhFlag = False
+        print('DHT22 not found')
+else:
+    dhFlag = False
+
+if sht30Pins is not None:
+    import sht30
+    sda, scl = sht30Pins
+    sense1 = sht30.SHT30(scl_pin=scl, sda_pin=sda)
+    sense2 = sht30.SHT30(scl_pin=scl, sda_pin=sda, i2c_address=sht30.ALTERNATE_I2C_ADDRESS)  
+    sht30Flag = sense1.is_present() | (sense2.is_present() << 1)
+    if not sht30Flag:
+        print('SHT30 sensor(s) not found')
+else:
+    sht30Flag = False
 
 relay = machine.Pin(15,machine.Pin.OUT)
 relay.value(0)
@@ -43,48 +103,70 @@ while ctlFlg:
         print("low battery, monitor only")
         ctlFlg = False
         sleepTime = 120000
-    # configure wakeup
-    rtc = machine.RTC()
-    rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-    rtc.alarm(rtc.ALARM0,sleepTime)
-
 
     #open client
     try:
-        c = MQTTClient(brdName,mqttHost,keepalive=30,user=userToken,password='')
+        cTb = MQTTClient(brdName,mqttHost,keepalive=30,user=userToken,password='')
         # extra delay to allow network to stabilize
-        time.sleep(1)           
-        c.connect()
+        time.sleep_ms(100)           
+        cTb.connect()
         print("connecting to broker")
         brokerFlg = True
     except OSError:
         print("failure to connect to broker")
         brokerFlg = False
-        ctlFlg = False
+
+    #for controller
+    if ctlFlg:
+        try:
+            cCtl = MQTTClient(brdName,mqttHost2)
+            cCtl.set_callback(relayCtl)
+            cCtl.connect()
+            print("connecting to controller")
+            cCtl.subscribe(mqttTopic2)
+        except OSError:
+            print("failure to connect to controller")
+            ctlFlg = False
 
 
-    time.sleep(1)           # allow connection setup and temperature read
+    time.sleep_ms(100)           # allow connection setup and temperature read
+    message = "{"
     milliVolts = vin_mV(adc.read,adcScl)
     vStr = milli_str(milliVolts)
-    message = "{'volts' : "+vStr
+    message += "'volts' : {}".format(vStr)
+    if tempFlg:
+        message += ", 'temperature' : {}".format(ds.read_temp(roms[0]))
+        if len(roms) > 1:
+            for ti in range(1,len(roms)):
+                message += ", 'temp{}' : {}".format(ti+2,ds.read_temp(roms[ti]))
     if sense1.is_present():
         t1,rh1 = sense1.measure()
-        message = message + ", 'temperature' : " + str(t1) + ", 'rh1' : " + str(rh1)
+        message += ", 'temp1' : {}, 'rh1' : {}".format(t1,rh1)
     if sense2.is_present():
         t2,rh2 = sense2.measure()
-        message = message + ", 'temp2' : " + str(t2) + ", 'rh2' : " + str(rh2)
-    message = message + "}"
+        message += ", 'temp2' : {}, 'rh2' : {}".format(t2,rh2)
+    if ctlFlg:
+        message += ", 'relayEcho' : {}".format(relay.value())
+    message += "}"
 
     if brokerFlg:
-        c.publish("v1/devices/me/telemetry",message)
-        time.sleep(1)
-        c.disconnect()
+        cTb.publish("v1/devices/me/telemetry",message)
+        time.sleep_ms(100)
+        cTb.disconnect()
+    else:
+        print(message)
 
 
     if not ctlFlg:
         print('back to sleep: '+milli_str(sleepTime))
         relay.value(0)
-        machine.deepsleep()     # back to sleep
+        machine.deepsleep(sleepTime)     # back to sleep
     else:
         print('awake but waiting: '+milli_str(sleepTime))
-        time.sleep(sleepTime/1000)
+        ts = 0
+        if tempFlg:
+            ds.convert_temp()
+        while (ts < sleepTime/500):
+            cCtl.check_msg()
+            time.sleep_ms(500)
+            ts += 1
