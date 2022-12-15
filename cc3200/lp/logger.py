b875e63c9bb4c83273fe67e899e742cfe8c1d957
Niall Parker <stuff@npengineering.ca>
2022-12-14 16:27:08 -0800

    generalize ADC reading, calibrate with filters

--- cc3200/lp/logger.py
+++ cc3200/lp/logger.py
@@ -33,10 +33,14 @@ def dieTemp_str(i2c,addr):
     dTemp = struct.unpack(">h",i2c.readfrom_mem(addr,1,2))[0]
     return str(dTemp // 128) + "." + str((dTemp % 128)*100 // 128)
 
-# account for voltage divider of 115k over 56k on Vbatt (on expansion board)
+# account for voltage divider, 100k over [3k3, 4k7, 10k]
 # calibrated against MS8209 DVM, np 16may2020
-def vbatt_str(adc):
-    milliVolts = (adc()*4722) // 4096
+def volt_str(ch,scale):
+    adc = machine.ADC().channel(ch)
+    acc = 0
+    for i in range(16):
+        acc += adc()
+    milliVolts = (acc*scale) >> (12+4)
     return str(milliVolts // 1000) + "." + str(milliVolts % 1000)
 
 def sw_str(switch):
@@ -61,10 +65,7 @@ if TMP006_ADDR in i2c.scan():
 sw_hi = Pin('GP13', mode=Pin.IN, pull=Pin.PULL_UP)
 sw_lo = Pin('GP22', mode=Pin.IN, pull=Pin.PULL_UP)
 
-adc = machine.ADC()
-vbatt24 = adc.channel(1)
-vbatt12 = adc.channel(2)
-vSolar = adc.channel(3)
+adc = { 'vB24' : (1,32660), 'vB12' : (2,16741), 'vSolar' : (3,44486) }
 
 pubCount = 0
 nextCount = pubCount
@@ -93,19 +94,14 @@ while True:
         print("connecting to thingsboard")
         tb_msg = "{"
         c.publish(brdName+"/iter",str(pubCount))
-        vSol_str = str(vSolar())
-        c.publish(brdName+"/vSolar",vSol_str)
-        tb_msg += "'vSolar' : {},".format(vSol_str)
+        for topic,chScl in adc.items():
+            mVolts = volt_str(chScl[0],chScl[1])
+            c.publish(brdName+"/"+topic,mVolts)
+            tb_msg += "'{}' : {},".format(topic,mVolts)
         if tempFlg:
             tempStr = dieTemp_str(i2c,TMP006_ADDR)
             tb_msg += "'temperature' : {},".format(tempStr)
             c.publish(brdName+"/temperature",tempStr)
-        vB24_str =str(vbatt24())
-        c.publish(brdName+"/vB24",vB24_str)
-        tb_msg += "'vB24' : {},".format(vB24_str)
-        vB12_str =str(vbatt12())
-        c.publish(brdName+"/vB12",vB12_str)
-        tb_msg += "'vB12' : {},".format(vB12_str)
         c.publish(brdName+"/sw_high",sw_str(sw_hi))
         c.publish(brdName+"/sw_low",sw_str(sw_lo))
         tb_msg += "'sw_high' : {}, 'sw_low' : {} }}".format(sw_str(sw_hi),sw_str(sw_lo))
