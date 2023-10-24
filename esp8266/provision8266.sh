daa52751a50e1d1d706bec043b45b516a34fae54
Niall Parker <stuff@npengineering.ca>
2023-10-24 14:58:06 -0700

    generalize provision script

--- esp8266/provision8266.sh
+++ esp8266/provision8266.sh
@@ -1,8 +1,39 @@
-ampy -p /dev/ttyUSB0 put boot.py
-ampy -p /dev/ttyUSB0 put boardCfg_minD1c.py boardCfg.py
-ampy -p /dev/ttyUSB0 put ../fileQueue.py
-ampy -p /dev/ttyUSB0 put station.py main.py
-ampy -p /dev/ttyUSB0 put ../netConfig.py
-ampy -p /dev/ttyUSB0 put ../sht30.py
-ampy -p /dev/ttyUSB0 put ../umqtt_simple.py
-ampy -p /dev/ttyUSB0 put ../ds18B20cal.py 
+#!/bin/bash
+if [ $# -eq 0 ]; then
+	echo "Usage $0 <board> <main> <port>"
+	exit 1
+fi
+if [ -z "$3" ]; then
+	export AMPY_PORT=/dev/ttyUSB0
+else
+	export AMPY_PORT=/dev/tty$3
+fi
+if [ -z "$2" ]; then
+	MAIN=logger
+else
+	MAIN=$2
+fi
+if [ -z "$1" ]; then
+	echo "Need to define board to program"
+	exit 2
+else
+	BOARD=$1
+fi
+echo "existing files:"
+ampy ls
+echo "replacing boot.py"
+ampy put boot.py
+echo "replacing boardCfg.py"
+ampy put boardCfg_$BOARD.py boardCfg.py
+echo "replacing fileQueue"
+ampy put ../fileQueue.py
+echo "replacing main"
+ampy put $MAIN.py main.py
+echo "replacing netConfig"
+ampy put ../netConfig.py
+echo "replacing sht30"
+ampy put ../sht30.py
+echo "replacing mqtt"
+ampy put ../umqtt_simple.py
+echo "replacing DS18B20 cal"
+ampy put ../ds18B20cal.py 
