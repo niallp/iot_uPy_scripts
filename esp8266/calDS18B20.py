f8f112ab9bd1062e17f9c5e0eefef4c5b34ef478
Niall Parker <stuff@npengineering.ca>
2024-04-16 19:55:47 -0700

    more DS18B20 sensors calibrated

--- esp8266/calDS18B20.py
+++ esp8266/calDS18B20.py
@@ -1,8 +1,4 @@
-# basic data logger using WiPy
-# fork for esp2866 modules
-# Niall Parker, 7apr2016
-# adapt for Thingsboard demo
-# isolate board specific values to boardCfg.py
+# util for display of DS18B20 data
 
 import socket
 import time
@@ -15,6 +11,7 @@ from umqtt_simple import MQTTClient
 from boardCfg import brdName
 from boardCfg import adcScl
 from boardCfg import userToken
+from netConfig import mqttHost
 from netConfig import RSSI
 from boardCfg import highPin
 from boardCfg import lowPin
@@ -48,56 +45,80 @@ def readDS(addr):
     else:
         return ds.read_temp(addr)
 
-#setup 
-adc = machine.ADC(0)
-milliVolts = vin_mV(adc.read,adcScl)    # want to sleep longer if voltage is low
-sleepTime = 10000
-
-brokerFlg = False
-
-if oneWirePin is not None:
-    import onewire
-    import ds18x20
-    ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(oneWirePin)))
-    roms = ds.scan()
-    if len(roms) == 0:
-        tempFlg = False
-        print('DS18x20 not found')
+def scanDS18B20(oneWirePin):
+    global ds
+    if oneWirePin is not None:
+        import onewire
+        import ds18x20
+        ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(oneWirePin)))
+        roms = ds.scan()
+        if len(roms) == 0:
+            print('DS18x20 not found')
+            return False
+        else:
+            ds.convert_temp()
+            return roms
     else:
+        return False
+
+def dispDS18B20(roms):
+    if len(roms) > 0:
+        message = "{"
+        message += "'temp{}' : {}".format(0,readDS(roms[0]))
+        for ti in range(1,len(roms)):
+            message += ", 'temp{}' : {}".format(ti,readDS(roms[ti]))
+        message += "}"
+        return message
+    else:
+        return ''
+
+def dispAvgOff(roms,n,offset):
+    global ds
+    accum = [0.0] * len(roms)
+    for i in range(n):
         ds.convert_temp()
-        tempFlg = True
-        print(roms)
+        time.sleep(1)
+        for ti in range(len(roms)):
+            accum[ti] += readDS(roms[ti])
+    print(''.join('{} '.format(accum[x]/n-offset) for x in range(len(roms))))
+
+def dispAvg(roms,n):
+    dispAvgOff(roms,n,0.0)
+
+#optional port for mqttHost
+if type(mqttHost) is tuple:
+    mqttHost, mqttPort = mqttHost
 else:
-    tempFlg = False
+    mqttPort = 0       # use default
 
-dhFlag = False
-sht30Flag = False
 
-for t in range(1000):
-    time.sleep(1)           # allow temperature read
+roms = scanDS18B20(oneWirePin)
+hdr = ''
+for r in roms:
+    hdr += ''.join('{:02x}'.format(x) for x in r)
+    hdr += ' '
+print(hdr)
+dispAvg(roms,10)
 
-    vStr = milli_str(milliVolts)
-    message = "{'volts' : "+vStr
-    if highPin is not None:
-        message = message + ", 'sw_high' : "+pin_str(highPin)
-    if lowPin is not None:
-        message = message + ", 'sw_low' : "+pin_str(lowPin)
-    if tempFlg:
-        message += ", 'temperature' : {}".format(readDS(roms[0]))
-        # any additional 1W sensors will carry on at temp3 etc.
-        if len(roms) > 1:
-                for ti in range(1,len(roms)):
-                    message += ", 'temp{}' : {}".format(ti+2,readDS(roms[ti]))
-    # only expect one of following RH sensors installed
-    if dhFlag:
-        message = message + ", 'temp2' : "+ str(ht.temperature()) + ", 'rh1' : "+ str(ht.humidity()) 
-    if sht30Flag:
-        t2, rh = sense.measure()
-        message = message + ", 'temp2' : "+ str(t2) + ", 'rh1' : "+ str(rh) 
-    message = message + "}"
+#open client
+try:
+    c = MQTTClient(brdName,mqttHost,port=mqttPort,keepalive=30,user=userToken,password='')
+    # extra delay to allow network to stabilize
+    time.sleep(1)           
+    c.connect()
+    print("connecting to broker")
+    brokerFlg = True
+except:
+    print("failure to connect to broker")
+    brokerFlg = False
 
-    if not brokerFlg:
-        print(message)
+for i in range(10000):
+    msg = dispDS18B20(roms)
+    ds.convert_temp()
+    print('{}:{}'.format(i,msg))
+    if brokerFlg:
+        c.publish("v1/devices/me/telemetry",msg)
+    time.sleep(10)
 
-    print('back to sleep: '+ milli_str(sleepTime))
-    time.sleep_ms(sleepTime)
+c.disconnect()
+
